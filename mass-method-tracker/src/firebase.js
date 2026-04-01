f6a5f549421892bc91ad0aa9437f7e8534c626f2
kjselch-sketch <kjselch@hotmail.com>
2026-03-31 21:11:10 -0500

    Update firebase.js

--- mass-method-tracker/src/firebase.js
+++ mass-method-tracker/src/firebase.js
@@ -1,22 +1,22 @@
-// Import the functions you need from the SDKs you need
-import { initializeApp } from "firebase/app";
-import { getAnalytics } from "firebase/analytics";
-// TODO: Add SDKs for Firebase products that you want to use
-// https://firebase.google.com/docs/web/setup#available-libraries
 
-// Your web app's Firebase configuration
-// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+import { initializeApp } from "firebase/app";
+import { getFirestore } from "firebase/firestore";
+import { getAuth, signInAnonymously } from "firebase/auth";
+
 const firebaseConfig = {
-  apiKey: "AIzaSyCiXW1Ium_ahCFMbfvRbiHCssIh_ixik9w",
-  authDomain: "massmethod.firebaseapp.com",
-  databaseURL: "https://massmethod-default-rtdb.firebaseio.com",
-  projectId: "massmethod",
-  storageBucket: "massmethod.firebasestorage.app",
+  apiKey:            "AIzaSyCiXW1Ium_ahCFMbfvRbiHCssIh_ixik9w",
+  authDomain:        "massmethod.firebaseapp.com",
+  databaseURL:       "https://massmethod-default-rtdb.firebaseio.com",
+  projectId:         "massmethod",
+  storageBucket:     "massmethod.firebasestorage.app",
   messagingSenderId: "550528083023",
-  appId: "1:550528083023:web:d51f001b52bd50f1cbf63a",
-  measurementId: "G-8Y56Y7EV1J"
+  appId:             "1:550528083023:web:d51f001b52bd50f1cbf63a",
+  measurementId:     "G-8Y56Y7EV1J"
 };
 
-// Initialize Firebase
 const app = initializeApp(firebaseConfig);
-const analytics = getAnalytics(app);
+export const db = getFirestore(app);
+
+// Auto sign-in anonymously so Firestore rules allow access
+export const auth = getAuth(app);
+signInAnonymously(auth);
